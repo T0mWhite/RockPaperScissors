4f8967b5d9d469acc16c63cd55ed2661d0872f89
Tommy White <tbw2.mail@gmail.com>
2022-02-18 07:43:28 -0700

    Game works for a single round, computer has bomb. Player does not have bomb.

--- index.js
+++ index.js
@@ -3,23 +3,25 @@
 // Player is playing a game against a computer.
 
 // On load, ask for player name
-const name = prompt('Please enter your name:')
+const name = prompt('Please enter your name:');
 console.log(name);
 // Send console greeting with player name and ask player if they would like to play rock, paper, scissors
-const answer = prompt(`Welcome ${name}, are you ready to play Rock Paper Scissors? Type yes or no.`);
+const answer = prompt(
+  `Welcome ${name}, are you ready to play Rock Paper Scissors? Type yes or no.`
+);
 
 // If yes, commence game
 if (answer.toLowerCase() === 'yes') {
-    // Game logic
-    let startRound = () => {
+  // Game logic
+  let startRound = () => {
     let compThrow;
     // Function to get the computer's throw
     function getCompThrow() {
-        compThrow = Math.floor(Math.random()*4 + 1);
-    } 
+      compThrow = Math.floor(Math.random() * 4 + 1);
+    }
     // Call the computer's throw fucntion
     getCompThrow();
-    
+
     // switch (compThrow) {
     //     case 1:
     //         compThrow = 'rock'
@@ -27,13 +29,13 @@ if (answer.toLowerCase() === 'yes') {
     // }
     // Set the computer's throw number to a string
     if (compThrow === 1) {
-        compThrow = 'rock'
+      compThrow = 'rock';
     } else if (compThrow === 2) {
-        compThrow = 'paper'
+      compThrow = 'paper';
     } else if (compThrow === 3) {
-        compThrow = 'scissors'
+      compThrow = 'scissors';
     } else if (compThrow === 4) {
-        compThrow = 'bomb'
+      compThrow = 'bomb';
     }
 
     // Ask the player for their throw
@@ -43,26 +45,39 @@ if (answer.toLowerCase() === 'yes') {
 
     // Compare the comp throw to the player throw
     if (compThrow === playerThrow) {
-        console.log(`Computer threw ${compThrow}, and you threw ${playerThrow}. The round was a tie!`);
+      console.log(
+        `Computer threw ${compThrow}, and you threw ${playerThrow}. The round was a tie!`
+      );
     } else if (compThrow === 'rock' && playerThrow !== 'paper') {
-        console.log(`Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`);
+      console.log(
+        `Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`
+      );
     } else if (compThrow === 'paper' && playerThrow !== 'scissors') {
-        console.log(`Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`);
+      console.log(
+        `Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`
+      );
     } else if (compThrow === 'scissors' && playerThrow !== 'rock') {
-        console.log(`Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`);
-    // } else if (compThrow !== playerThrow && )
-    return console.log(`Computer threw ${compThrow}, and you threw ${playerThrow}. Nice! You've won this round!`);
-  }
+      console.log(
+        `Computer threw ${compThrow}, and you threw ${playerThrow}. You've lost this round!`
+      );
+    } else if (compThrow === 'bomb') {
+      console.log(
+        `Computer threw ${compThrow}! Computer blew you up! Game over!`
+      );
+    } else {
+      return console.log(
+        `Computer threw ${compThrow}, and you threw ${playerThrow}. Nice! You've won this round!`
+      );
+    }
+  };
 
   startRound();
 
-    // If no, clear console and end game
+  // If no, clear console and end game
 } else if (answer.toLowerCase() === 'no') {
-    console.clear();
-    endGame();
+  console.clear();
+  endGame();
 }
-
-// Computer's throw is generated, options are "rock", "paper", "scissors", or "bomb".
 
 // Player is asked to send their throw through the console.
 // Player commands can be "rock", "paper", "scissors", or "bomb". Commands are not case sensitive.
